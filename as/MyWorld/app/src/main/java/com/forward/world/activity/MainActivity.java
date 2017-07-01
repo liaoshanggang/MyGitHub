a0bb6acf7faa2966e05e4d460e492beeee279708
liaoshanggang <787887060@qq.com>
2017-07-01 12:12:30 +0800

    Main tag_bottom layout

--- as/MyWorld/app/src/main/java/com/forward/world/activity/MainActivity.java
+++ as/MyWorld/app/src/main/java/com/forward/world/activity/MainActivity.java
@@ -2,14 +2,20 @@ package com.forward.world.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.RadioGroup;
 
 import com.forward.world.R;
 
 public class MainActivity extends Activity {
-
+    private FrameLayout fl_main_content;
+    private RadioGroup rg_bottom_tag;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fl_main_content = (FrameLayout) findViewById(R.id.fl_main_content);
+        rg_bottom_tag = (RadioGroup) findViewById(R.id.rg_bottom_tag);
+        rg_bottom_tag.check(R.id.rd_video);
     }
 }
