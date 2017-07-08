1743d6f5120d496e31d5ea47829dd737b25e102d
liaoshanggang <787887060@qq.com>
2017-07-08 21:22:18 +0800

    nothing

---
+++ as/MyWorld/app/src/main/java/com/forward/world/view/TitleBar.java
@@ -0,0 +1,24 @@
+package com.forward.world.view;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by liaoshanggang on 7/8/2017.
+ */
+
+public class TitleBar extends LinearLayout{
+    public TitleBar(Context context) {
+        super(context);
+    }
+
+    public TitleBar(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TitleBar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
