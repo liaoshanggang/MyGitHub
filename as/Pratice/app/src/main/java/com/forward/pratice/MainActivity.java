7d12288cf4040fd484da5b218afa9a6042bcb9a1
liaoshanggang <787887060@qq.com>
2017-07-13 12:48:16 +0800

    cookietest/.gitignore

--- as/Pratice/app/src/main/java/com/forward/pratice/MainActivity.java
+++ as/Pratice/app/src/main/java/com/forward/pratice/MainActivity.java
@@ -2,12 +2,40 @@ package com.forward.pratice;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    /*logt*/
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /*Ctrl+o重写方法*/
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(this,"I'm the add_item",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this,"I'm the remove_item",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.show_menu:
+                Toast.makeText(this,"I'm show_menu",Toast.LENGTH_SHORT).show();
+            default:
+                break;
+        }
+        return true;
     }
 }
