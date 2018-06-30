2ab05009a874e257b8f1123820cca2ff71214861
liaoshanggang <787887060@qq.com>
2018-06-30 11:45:20 +0800

    eureka-server

---
+++ IDEA/forward/src/main/java/com/forward/ForwardApplication.java
@@ -0,0 +1,12 @@
+package com.forward;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForwardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForwardApplication.class, args);
+	}
+}
